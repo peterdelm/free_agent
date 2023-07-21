fdbfb4a830732b8fc1f48e57ba0bef6de76314f6
Peter Del Mastro <72809827+CabbageConundrum@users.noreply.github.com>
2023-07-21 18:11:23 -0400

    messing with login auth

--- components/screens/WelcomeScreen.js
+++ components/screens/WelcomeScreen.js
@@ -15,20 +15,20 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Styles from "./Styles";
 import PropTypes from "prop-types";
 
-function WelcomeScreen({ navigation, setToken }) {
+function WelcomeScreen({ navigation }) {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [token, setToken] = useState();
+  const [token, setToken] = useState("");
 
   //Remove this into a config file vvv
   const url = "http://192.168.0.11:3001/api/users";
 
-  if (!token) {
-    return <Login setToken={setToken} />;
-  }
-  Login.propTypes = {
-    setToken: PropTypes.func.isRequired,
-  };
+  // if (!token) {
+  //   return <Login setToken={setToken} />;
+  // }
+  // Login.propTypes = {
+  //   setToken: PropTypes.func.isRequired,
+  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -40,18 +40,39 @@ function WelcomeScreen({ navigation, setToken }) {
   };
 
   const sendLoginRequest = (credentials) => {
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credentials),
-    }).then((res) => {
+    console.log("handleLoginRequest called");
+
+    const url = "http://192.168.0.11:3001/api/users";
+    fetch(url).then((res) => {
       if (res.ok) {
-        return res.json();
+        data = res.json();
+        console.log(data.token);
+        return data.token;
       }
       throw new Error("Network response was not ok.");
     });
+    // fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(credentials),
+    // }).then((res) => {
+    //   if (res.ok) {
+    //     return res.json();
+    //   }
+    //   throw new Error("Network response was not ok.");
+    // });
+  };
+
+  const handleSuccessfulLogin = () => {
+    navigation.navigate("Home");
+  };
+
+  const handleLoginAttempt = (credentials) => {
+    console.log("handleLoginAttempt called");
+    loginRequestResult = sendLoginRequest(credentials);
+    setToken(loginRequestResult);
   };
 
   return (
@@ -81,9 +102,13 @@ function WelcomeScreen({ navigation, setToken }) {
 
         <TouchableOpacity>
           <Text style={Styles.forgotButton}>Forgot Password?</Text>
+          <Text style={Styles.forgotButton}>{token}</Text>
         </TouchableOpacity>
         <TouchableOpacity>
-          <Button title="LOGIN" onPress={() => navigation.navigate("Home")} />
+          <Button
+            title="LOGIN"
+            onPress={() => handleLoginAttempt(email, password)}
+          />
         </TouchableOpacity>
         <TouchableOpacity>
           <Text style={Styles.registerButton}>REGISTER</Text>
