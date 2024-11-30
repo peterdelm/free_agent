7b19f2d1ecddfa1860b5aec4d50588d4180a7188
peterdelm <peterdelmastro@hotmail.com>
2024-11-30 11:59:03 -0500

    removed hardcoded date from map markers

--- components/screens/MapComponent.js
+++ components/screens/MapComponent.js
@@ -1,8 +1,7 @@
 import React from "react";
 import MapView, { Marker, PROVIDER_GOOGLE, Callout } from "react-native-maps";
 import { StyleSheet, View, Text } from "react-native";
-import Styles from "./Styles"; // Ensure Styles has the necessary styles
-
+import Styles from "./Styles";
 export default function MapComponent({
   navigation,
   activeGames = [],
@@ -17,22 +16,45 @@ export default function MapComponent({
     longitudeDelta: 0.1,
   };
 
-  const formatGameDate = (rawDate, time) => {
-    const date = new Date(rawDate);
+  const formatGameDate = (isoDateString, time) => {
+    let date = new Date(isoDateString);
+
+    if (isNaN(date)) {
+      console.error("Invalid date:", isoDateString);
+      date = new Date(`1970-01-01T${time}`);
+    }
     const month = date.toLocaleString("en-US", { month: "long" });
     const day = date.getDate();
 
-    const timeDate = new Date(`1970-01-01T${time}`);
-    const hours = timeDate.getHours();
-    const minutes = timeDate.getMinutes();
-    const formattedTime = `${hours}:${minutes.toString().padStart(2, "0")}`;
+    const formattedTime = formatTime(time);
     return `${month} ${day} @ ${formattedTime}`;
+  };
+
+  const parse24HourTime = (timeString) => {
+    const [hours, minutes, seconds] = timeString.split(":").map(Number);
+    return { hours, minutes, seconds };
+  };
+  // Function to convert 24-hour time to 12-hour format
+  const convertTo12HourFormat = (hours) => {
+    const amPm = hours >= 12 ? "PM" : "AM";
+    const hours12 = hours % 12 || 12; // Convert 0 hours to 12 for AM
+    return { hours12, amPm };
+  };
+
+  // Main function to format time string
+  const formatTime = (timeString) => {
+    if (!/^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/.test(timeString)) {
+      return "Invalid time"; // Return a fallback message or value
+    }
+    const { hours, minutes } = parse24HourTime(timeString);
+    const { hours12, amPm } = convertTo12HourFormat(hours);
+    return `${hours12}:${String(minutes).padStart(2, "0")} ${amPm}`;
   };
 
   const markers = activeGames.map(({ game }, index) => {
     const latitude = game.geocoordinates?.latitude || 0;
     const longitude = game.geocoordinates?.longitude || 0;
-    const formattedDate = formatGameDate("2024-12-12T00:00:00.000Z", game.time);
+    const formattedDate = formatGameDate(game.date, game.time);
     const description = `${formattedDate}\n${game.location}`;
 
     return {
