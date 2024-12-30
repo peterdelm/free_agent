67d7a6777b60b1ce9c31f77b2bfbf74af963da9d
peterdelm <peterdelmastro@hotmail.com>
2024-12-30 13:58:50 -0500

    added map clustering

--- components/screens/MapComponent.js
+++ components/screens/MapComponent.js
@@ -1,12 +1,18 @@
-import React from "react";
-import MapView, { Marker, PROVIDER_GOOGLE, Callout } from "react-native-maps";
+import { useRef, useState, useEffect } from "react";
+import { Marker, PROVIDER_GOOGLE, Callout } from "react-native-maps";
+import MapView from "react-native-map-clustering";
 import { StyleSheet, View, Text } from "react-native";
 import Styles from "./Styles";
+import ClusterViewModal from "./clusterViewModal.js";
+
 export default function MapComponent({
   navigation,
   activeGames = [],
   playerLocation = {},
 }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedCluster, setSelectedCluster] = useState([]);
+
   const startingLongitude = playerLocation?.longitude || -79.347015;
   const startingLatitude = playerLocation?.latitude || 43.65107;
   const initialRegion = {
@@ -14,6 +20,13 @@ export default function MapComponent({
     longitude: startingLongitude,
     latitudeDelta: 0.1,
     longitudeDelta: 0.1,
+  };
+
+  const openModal = () => {
+    setIsModalVisible(true);
+  };
+  const closeModal = () => {
+    setIsModalVisible(false);
   };
 
   const formatGameDate = (isoDateString, time) => {
@@ -34,17 +47,16 @@ export default function MapComponent({
     const [hours, minutes, seconds] = timeString.split(":").map(Number);
     return { hours, minutes, seconds };
   };
-  // Function to convert 24-hour time to 12-hour format
+
   const convertTo12HourFormat = (hours) => {
     const amPm = hours >= 12 ? "PM" : "AM";
     const hours12 = hours % 12 || 12; // Convert 0 hours to 12 for AM
     return { hours12, amPm };
   };
 
-  // Main function to format time string
   const formatTime = (timeString) => {
     if (!/^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/.test(timeString)) {
-      return "Invalid time"; // Return a fallback message or value
+      return "Invalid time";
     }
     const { hours, minutes } = parse24HourTime(timeString);
     const { hours12, amPm } = convertTo12HourFormat(hours);
@@ -62,6 +74,8 @@ export default function MapComponent({
       gameId: game.id,
       title: `${game.gameType} ${game.sport}`,
       date: formattedDate,
+      rawDate: game.date,
+      sport: game.sport,
       address: game.location,
       description: description,
       coordinate: { latitude, longitude },
@@ -77,15 +91,33 @@ export default function MapComponent({
     navigation.navigate("ViewGame", { gameId });
   };
 
+  const handleClusterPress = async (cluster, markers) => {
+    setSelectedCluster(markers);
+    openModal();
+    console.log("Cluster Pressed:", cluster);
+    console.log("Markers in cluster:", markers);
+  };
+
   return (
     <View style={Styles.playerHomeMapContainer}>
-      <MapView style={styles.map} region={initialRegion}>
+      <MapView
+        style={styles.map}
+        region={initialRegion}
+        onClusterPress={(cluster, markers) =>
+          handleClusterPress(cluster, markers)
+        }
+        preserveClusterPressBehavior={true}
+      >
         {markers.map((marker) => (
           <Marker
-            key={marker.gameId} // Use unique key
+            key={marker.gameId}
+            gameId={marker.gameId}
             coordinate={marker.coordinate}
             description={marker.description}
+            date={marker.date}
+            sport={marker.sport}
             provider={PROVIDER_GOOGLE}
+            rawDate={marker.rawDate}
             onPress={() => handleMarkerPress(marker.gameId)}
           >
             <Callout onPress={() => handleCalloutPress(marker.gameId)}>
@@ -98,6 +130,13 @@ export default function MapComponent({
           </Marker>
         ))}
       </MapView>
+      <ClusterViewModal
+        isModalVisible={isModalVisible}
+        activeGames={selectedCluster}
+        // handleButtonPress={}
+        navigation={navigation}
+        onClose={closeModal}
+      />
     </View>
   );
 }
